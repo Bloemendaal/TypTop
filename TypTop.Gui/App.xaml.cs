86675e7c0e87c6fdc5ab6c9b8022a3ccd1a2c55d
janfokkeurk <janfokkeurk@gmail.com>
2019-11-13 14:53:05 +0100

    Removed monogame

---
+++ TypTop.Gui/App.xaml.cs
@@ -0,0 +1,17 @@
+***REMOVED***
+***REMOVED***
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TypTop.Gui
+***REMOVED***
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    ***REMOVED***
+***REMOVED***
+***REMOVED***
