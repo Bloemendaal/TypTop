03f861c35672d2a5e7e4c7a83b7d99f3dcf0c9df
janfokkeurk <janfokkeurk@gmail.com>
2019-11-13 13:47:22 +0100

    Removed monogame example code

--- TypTop.Game/MainWindowViewModel.cs
+++ TypTop.Game/MainWindowViewModel.cs
@@ -12,11 +12,6 @@ namespace TypTop.Game
     ***REMOVED***
         private readonly MainWindow _mainWindow;
         private SpriteBatch _spriteBatch;
-        private Texture2D _texture;
-        private Vector2 _position;
-        private float _rotation;
-        private Vector2 _origin;
-        private Vector2 _scale;
 
         public MainWindowViewModel(MainWindow mainWindow)
         ***REMOVED***
@@ -26,8 +21,6 @@ namespace TypTop.Game
         public override void LoadContent()
         ***REMOVED***
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _texture = Content.Load<Texture2D>("monogame-logo");
-
             _font = Content.Load<SpriteFont>("DefaultFont");
     ***REMOVED***
 
@@ -47,10 +40,7 @@ namespace TypTop.Game
 
         public override void Update(GameTime gameTime)
         ***REMOVED***
-            _position = GraphicsDevice.Viewport.Bounds.Center.ToVector2();
-            _rotation = (float)Math.Sin(gameTime.TotalGameTime.TotalSeconds);
-            _origin = _texture.Bounds.Center.ToVector2();
-            _scale = Vector2.One;
+            
     ***REMOVED***
 
         public override void Draw(GameTime gameTime)
@@ -58,7 +48,6 @@ namespace TypTop.Game
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(_texture, _position, null, Color.White, _rotation, _origin, _scale, SpriteEffects.None, 0f);
             _spriteBatch.DrawString(_font, _total??"Typ om te beginnen", new Vector2(30,30), Color.Black);
             _spriteBatch.End();
     ***REMOVED***
