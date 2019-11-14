cfba66c37ebcb6e5b38bb4854350eb8a0f32a666
GeorgiusNL <yourisl1998@gmail.com>
2019-11-14 13:41:46 +0100

    added properties, methods and comments to SpaceGame, Player and Enemy classes

--- TypTop.Game/SpaceGame/Enemy.cs
+++ TypTop.Game/SpaceGame/Enemy.cs
@@ -12,10 +12,18 @@ namespace TypTop.Game.SpaceGame
         public int X ***REMOVED*** get; private set; ***REMOVED***  //horizontal position of enemy in field
         public int Y ***REMOVED*** get; private set; ***REMOVED***  //vertical position of enemy in field
         public int Score ***REMOVED*** get; private set; ***REMOVED***  //amount of points the enemy represents
+        public int Speed ***REMOVED*** get; private set; ***REMOVED***  //amount of pixels the enemy moves each step
 
-        public Enemy()
+        public Enemy(int speed)
         ***REMOVED***
-
+            Word = "word";  // set word
+            X = 200;    // horizontal starting point
+            Y = 0;      // vertical starting point
+            Speed = speed;  // set speed
+            Score = Word.Length * Speed;    // set base score
     ***REMOVED***
+
+        // lets the enemy move with the designated speed each step
+        public void Move() => Y+= Speed;    
 ***REMOVED***
 ***REMOVED***
