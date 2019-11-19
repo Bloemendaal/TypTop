68bf2d944ece1458efa4788a2c55d9713dfd5aca
GeorgiusNL <yourisl1998@gmail.com>
2019-11-19 11:57:46 +0100

    unit tests file for spacegame window added

--- TypTop.Gui/SpaceGame/SpaceGameWindow.xaml.cs
+++ TypTop.Gui/SpaceGame/SpaceGameWindow.xaml.cs
@@ -17,18 +17,18 @@ namespace TypTop.Gui.SpaceGame
     /// </summary>
     public partial class SpaceGameWindow : Window
     {
-        SpaceGame Game = new SpaceGame(); // create spacegame object
+        public SpaceGame Game { get; set; } = new SpaceGame(); // create spacegame object
 
         // the following rectangle represents the player
-        Rectangle PlayerRect = new Rectangle
+        public Rectangle PlayerRect { get; set; } = new Rectangle
         {
             Name = "Player",
             Width = 50,
             Height = 50,
             Fill = Brushes.DarkCyan
-        };
+        }; 
 
-        List<Rectangle> EnemyRectList = new List<Rectangle>();  // list of rectangles for the enemies, should never countain more than largest what is shown at once on screen
+        public List<Rectangle> EnemyRectList = new List<Rectangle>();  // list of rectangles for the enemies, should never countain more than largest what is shown at once on screen
 
         public SpaceGameWindow()
         {
