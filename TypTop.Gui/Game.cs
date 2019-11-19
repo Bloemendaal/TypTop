df2dc0233641054a900390975a0781dbc64b6443
Casper Bloemendaal <casperbloemendaal@gmail.com>
2019-11-19 12:02:36 +0100

    Game init

--- TypTop.Gui/Game.cs
+++ TypTop.Gui/Game.cs
@@ -5,8 +5,19 @@ using System.Windows.Threading;
 
 namespace TypTop.Gui
 {
-    public class Game
+    public abstract class Game
     {
-        public DispatcherTimer Timer { get; } = new DispatcherTimer();  // timer
+        public readonly DispatcherTimer Timer = new DispatcherTimer();  // timer
+        public readonly Random Rnd = new Random(DateTime.Now.Millisecond);
+
+        public Game()
+        {
+            //  DispatcherTimer setup, ~59 intervals per second, 1000 / 60 = 16.6667... miliseconds
+            Timer.Tick += new EventHandler(Timer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 17);
+            Timer.Start();
+        }
+
+        protected abstract void Timer_Tick(object sender, EventArgs e);
     }
 }
