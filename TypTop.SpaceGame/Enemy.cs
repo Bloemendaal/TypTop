f74556036d5d97a52b91ce13bab8545961f8ca10
Sem Spanhaak <semspanhaak@gmail.com>
2019-11-25 20:36:37 +0100

    Added some positioning

--- TypTop.SpaceGame/Enemy.cs
+++ TypTop.SpaceGame/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 using System.Windows.Media.Imaging;
 using BasicGameEngine;
@@ -13,16 +14,20 @@ namespace TypTop.SpaceGame
         public Word Word { get; private set; }
         public int Speed { get; set; }
         private PositionComponent _positionComponent;
-        private VelocityComponent _velocityComponent;
 
-        public Enemy(int speed, Word word, Game game) : base("Enemy", game)
+        public Enemy(int speed, Word word, string name, Game game) : base(name, game)
         {
-            _positionComponent = new PositionComponent();
-            _velocityComponent = new VelocityComponent();
+            
+            _positionComponent = new PositionComponent()
+            {
+                Position = new Vector2(game.Rnd.Next(200, 1720), 0)
+            };
             AddComponent(_positionComponent);
-            AddComponent(_velocityComponent);
-            AddComponent(
-                new ImageComponent(new BitmapImage(new Uri(@"Images/enemy.png", UriKind.Relative))));
+            AddComponent(new VelocityComponent()
+            {
+                Velocity = new Vector2(0, 1)
+            });
+            AddComponent(new ImageComponent(new BitmapImage(new Uri(@"Images/enemy.png", UriKind.Relative))));
             Word = word;
             Speed = speed;
         }
