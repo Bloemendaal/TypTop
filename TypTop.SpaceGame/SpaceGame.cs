efa5dd8ff4070b25c2846a3520537c77997b3308
Sem Spanhaak <semspanhaak@gmail.com>
2019-11-25 14:07:49 +0100

    Added base

--- TypTop.SpaceGame/SpaceGame.cs
+++ TypTop.SpaceGame/SpaceGame.cs
@@ -1,14 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Windows.Shapes;
 using BasicGameEngine;
 using BasicGameEngine.GameEngine.Components;
+using TypTop.Gui.SpaceGame;
 
 namespace TypTop.SpaceGame
 {
     public class SpaceGame : Game
     {
+        public Player Player { get; set; }
+        public Queue<Enemy> EnemyQueue { get; set; }
+        public int LineHeight { get; set; }
+        public Level Level { get; set; }    
         public SpaceGame()
         {
-
+            Level = new Level(1);
+            Player = new Player();
+            EnemyQueue = new Queue<Enemy>();
+            LineHeight = 400;
         }
     }
 }
