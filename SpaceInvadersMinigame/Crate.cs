ef8add965fcd362598977b87e512a3ea84beec8e
janfokkeurk <janfokkeurk@gmail.com>
2019-11-25 11:21:45 +0100

    update

--- SpaceInvadersMinigame/Crate.cs
+++ SpaceInvadersMinigame/Crate.cs
@@ -6,24 +6,31 @@ using BasicGameEngine.GameEngine.Components;
 
 namespace SpaceInvadersMinigame
 ***REMOVED***
+
+
     public class Crate : Entity
     ***REMOVED***
         private readonly TransformComponent _transformComponent;
 
-        public Crate(Game game) : base("crate", game)
+        public Crate(string name, Game game) : base(name, game)
         ***REMOVED***
             _transformComponent = new TransformComponent();
             AddComponent(_transformComponent);
             var collisionComponent = new CollisionComponent(new Size(150, 150));
             AddComponent(collisionComponent);
             AddComponent(new ImageComponent(new BitmapImage(new Uri(@"krat.png", UriKind.Relative))));
-
             collisionComponent.Collision += CollisionComponentOnCollision;
     ***REMOVED***
 
         private void CollisionComponentOnCollision(object? sender, CollisionEventArgs e)
         ***REMOVED***
-            _transformComponent.Position -= e.PenetrationVector;
+            if (e.Entity is Crate)
+            ***REMOVED***
+                _transformComponent.Position -= e.PenetrationVector / 2;
+
+        ***REMOVED***
+            else
+                _transformComponent.Position -= e.PenetrationVector;
     ***REMOVED***
 ***REMOVED***
 ***REMOVED***
--- SpaceInvadersMinigame/Player.cs
+++ SpaceInvadersMinigame/Player.cs
@@ -11,7 +11,7 @@ namespace SpaceInvadersMinigame
     ***REMOVED***
         public Player(Game game) : base("player", game)
         ***REMOVED***
-            AddComponent(new TransformComponent());
+            AddComponent(new TransformComponent() ***REMOVED***Speed = 50***REMOVED***);
             AddComponent(new KeyboardMoveComponent());
             AddComponent(new CollisionComponent(new Size(150,150)));
             AddComponent(new ImageComponent(new BitmapImage(new Uri(@"player.png", UriKind.Relative))));
