525ec0903b0f6a879b72da0437967ac76623a429
Sem Spanhaak <semspanhaak@gmail.com>
2019-11-25 13:16:07 +0100

    init spacegame engine

---
+++ TypTop.SpaceGame/SpaceGame.cs
@@ -0,0 +1,14 @@
+using System;
+using BasicGameEngine;
+using BasicGameEngine.GameEngine.Components;
+
+namespace TypTop.SpaceGame
+{
+    public class SpaceGame : Game
+    {
+        public SpaceGame()
+        {
+
+        }
+    }
+}
