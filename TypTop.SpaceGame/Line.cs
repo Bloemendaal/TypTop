e41a6cf964db878176a28eae13ae6e6b1687ad63
Sem Spanhaak <semspanhaak@gmail.com>
2019-11-26 19:56:02 +0100

    Added background and line.

---
+++ TypTop.SpaceGame/Line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using BasicGameEngine;
+using BasicGameEngine.GameEngine.Components;
+
+namespace TypTop.SpaceGame
+{
+    public class Line : Entity
+    {
+        private PositionComponent _positionComponent;
+        public Line(Game game) : base("Line", game)
+        {
+            _positionComponent = new PositionComponent()
+            {
+                Position = new Vector2(0, 900)
+            };
+            AddComponent(_positionComponent);
+            AddComponent(new LineComponent());
+        }
+    }
+}
--- TypTop.SpaceGame/SpaceGame.cs
+++ TypTop.SpaceGame/SpaceGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Windows.Shapes;
 using BasicGameEngine;
 using BasicGameEngine.GameEngine.Components;
+using Typop.SpaceGame;
 using TypTop.Gui.SpaceGame;
 using TypTop.Logic;
 
@@ -12,20 +13,22 @@ namespace TypTop.SpaceGame
     {
         public Player Player { get; set; }
         public Queue<Enemy> EnemyQueue { get; set; }
-        public int LineHeight { get; set; }
         public Level Level { get; set; }
         public SpaceGame()
         {
             Level = new Level(1, this);
             Player = new Player(this);
             EnemyQueue = new Queue<Enemy>();
-            LineHeight = 400;
-            AddEntity(Player);
+
+            
+            AddEntity(new Background(this));
+            
             foreach (var enemy in Level.EnemyList)
             {
                 AddEntity(enemy);
             }
-            //AddEntity(new Enemy(1, new Word("das"), "Enemy",this));
+            AddEntity(Player);
+            AddEntity(new Line(this));
         }
     }
 }
