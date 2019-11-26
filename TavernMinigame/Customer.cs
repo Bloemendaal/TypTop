c8bb71cf5f798f2aca0bb58e48849734bc3803e7
Casper Bloemendaal <casperbloemendaal@gmail.com>
2019-11-26 16:32:23 +0100

    variable width

--- TavernMinigame/Customer.cs
+++ TavernMinigame/Customer.cs
@@ -10,9 +10,9 @@ namespace TavernMinigame
     public class Customer : Entity
     ***REMOVED***
         private List<Order> _orders;
-        public Customer(Game game) : base(game)
+        public Customer(List<Order> orders, Game game) : base(game)
         ***REMOVED***
-            
+            _orders = orders;
     ***REMOVED***
 
         public Order GetOrder(Order.OrderType orderType) => _orders.Where(o => o.Type == orderType).First();
