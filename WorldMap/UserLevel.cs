bf6e1e9b21157b9fa16a55a5937e4b93a85a8ddf
GeorgiusNL <yourisl1998@gmail.com>
2019-11-28 15:19:24 +0100

    added worldmap project with classes with properties and constructors

---
+++ WorldMap/UserLevel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldMap
+{
+    public class UserLevel
+    {
+        public int User { get; private set; }
+        public int Level { get; private set; }
+        public int Score { get; private set; }
+        public int Stars { get; private set; }
+
+        public UserLevel(User user, Level level)
+        {
+            User = user.Id;
+            Level = level.Id;
+
+            /* from database
+
+            Score = ;
+            Stars = ;
+
+            */
+        }
+    }
+}
