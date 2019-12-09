3e2a3c02f5d007853a1cef01b1b5ff3aa87823ae
GeorgiusNL <yourisl1998@gmail.com>
2019-12-09 10:57:04 +0100

    worldscreen & levelscreen

---
+++ LevelScreen/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace LevelScreen
+{
+    public class Class1
+    {
+    }
+}
---
+++ WorldScreen/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace WorldScreen
+{
+    public class Class1
+    {
+    }
+}
