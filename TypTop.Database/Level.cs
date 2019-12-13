d75952946bcec899eef599b8e0194183fd3c4dc8
nsrid <44698806+nsrid@users.noreply.github.com>
2019-12-13 11:38:26 +0100

    update-maybe wrong

---
+++ TypTop.Database/Level.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TypTop.Database
+{
+    public class Level
+    {
+        [Key]
+        public int LevelId { get; set; }
+
+        [Required]
+        public int WorldId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public int Index { get; set; }
+
+        [Required]
+        public double X { get; set; }
+
+        [Required]
+        public double Y { get; set; }
+
+    }
+}
---
+++ TypTop.Database/LevelUnlock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypTop.Database
+{
+    public class LevelUnlock
+    {
+        public int LevelId { get; set; }
+        public int RequiredId { get; set; }
+    }
+}
