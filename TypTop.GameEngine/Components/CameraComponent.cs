7466cca4a539f3e889b54adf8acc9cc026502043
Bloemendaal <casperbloemendaal@gmail.com>
2019-12-14 23:36:37 +0100

    Camera

---
+++ TypTop.GameEngine/Components/CameraComponent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace TypTop.GameEngine.Components
+{
+    public class CameraComponent : Component
+    {
+        public Vector2 Position
+        {
+            get => _position.TryGetValue(Entity.Game, out Vector2 result) ? result : new Vector2(0,0);
+            set
+            {
+                if (_position.ContainsKey(Entity.Game))
+                {
+                    _position[Entity.Game] = value;
+                }
+            }
+        }
+        private readonly static Dictionary<Game, Vector2> _position = new Dictionary<Game, Vector2>();
+
+        public float X
+        {
+            get => _position.TryGetValue(Entity.Game, out Vector2 result) ? result.X : 0;
+            set
+            {
+                _position.TryGetValue(Entity.Game, out Vector2 result);
+                result.X = value;
+            }
+        }
+        public float Y
+        {
+            get => _position.TryGetValue(Entity.Game, out Vector2 result) ? result.Y : 0;
+            set
+            {
+                _position.TryGetValue(Entity.Game, out Vector2 result);
+                result.Y = value;
+            }
+        }
+
+        public override void AddedToEntity()
+        {
+            if (!_position.ContainsKey(Entity.Game))
+            {
+                _position.Add(Entity.Game, new Vector2(0, 0));
+            }
+
+            base.AddedToEntity();
+        }
+    }
+}
--- TypTop.GameEngine/Components/PositionComponent.cs
+++ TypTop.GameEngine/Components/PositionComponent.cs
@@ -4,19 +4,18 @@ namespace TypTop.GameEngine.Components
 {
     public class PositionComponent : Component
     {
+        private CameraComponent _camera;
+
         public Vector2 Position
         {
-            get => _position;
-            set
-            {
-                _position = value;
-            }
+            get => _position - (_camera?.Position ?? new Vector2(0, 0));
+            set => _position = value;
         }
         private Vector2 _position;
 
         public float X
         {
-            get => Position == null ? 0 : Position.X;
+            get => (Position == null ? 0 : Position.X) - (_camera?.X ?? 0);
             set
             {
                 if (_position == null)
@@ -32,7 +31,7 @@ namespace TypTop.GameEngine.Components
         }
         public float Y
         {
-            get => Position == null ? 0 : Position.Y;
+            get => (Position == null ? 0 : Position.Y) - (_camera?.Y ?? 0);
             set
             {
                 if (_position == null)
@@ -54,5 +53,14 @@ namespace TypTop.GameEngine.Components
             Position = position;
         }
 
+        public override void AddedToEntity()
+        {
+            if (Entity.HasComponent<CameraComponent>())
+            {
+                _camera = Entity.GetComponent<CameraComponent>();
+            }
+
+            base.AddedToEntity();
+        }
     }
 }
