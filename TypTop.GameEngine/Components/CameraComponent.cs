229910e5369fda7d9712eb154f843bb9d541daa2
Bloemendaal <casperbloemendaal@gmail.com>
2019-12-15 10:37:38 +0100

    Update CameraComponent.cs

--- TypTop.GameEngine/Components/CameraComponent.cs
+++ TypTop.GameEngine/Components/CameraComponent.cs
@@ -10,7 +10,7 @@ namespace TypTop.GameEngine.Components
         /// </summary>
         public Vector2 Position
         {
-            get => _position.TryGetValue(Entity.Game, out Vector2 result) ? result : new Vector2(0,0);
+            get => _position.TryGetValue(Entity.Game, out Vector2 result) ? result : new Vector2(0, 0);
             set
             {
                 if (_position.ContainsKey(Entity.Game))
@@ -101,6 +101,70 @@ namespace TypTop.GameEngine.Components
         {
             _position.TryGetValue(game, out Vector2 vector2);
             return vector2;
+        }
+
+
+        /// <summary>
+        /// Set X by key.
+        /// </summary>
+        /// <param name="x">
+        /// X coordinate.
+        /// </param>
+        /// <param name="game">
+        /// TKey of the Dictionary.
+        /// </param>
+        public static void SetX(float x, Game game)
+        {
+            if (_position.TryGetValue(game, out Vector2 vector2))
+            {
+                vector2.X = x;
+            }
+            else
+            {
+                _position.Add(game, new Vector2(x, 0));
+            }
+        }
+
+        /// <summary>
+        /// Set Y by key.
+        /// </summary>
+        /// <param name="y">
+        /// Y coordinate.
+        /// </param>
+        /// <param name="game">
+        /// TKey of the Dictionary.
+        /// </param>
+        public static void SetY(float y, Game game)
+        {
+            if (_position.TryGetValue(game, out Vector2 vector2))
+            {
+                vector2.Y = y;
+            }
+            else
+            {
+                _position.Add(game, new Vector2(0, y));
+            }
+        }
+
+        /// <summary>
+        /// Set Position by key.
+        /// </summary>
+        /// <param name="position">
+        /// Vector2 coordinates.
+        /// </param>
+        /// <param name="game">
+        /// TKey of the Dictionary.
+        /// </param>
+        public static void SetPosition(Vector2 position, Game game)
+        {
+            if (_position.ContainsKey(game))
+            {
+                _position[game] = position;
+            }
+            else
+            {
+                _position.Add(game, position);
+            }
         }
 
         public override void AddedToEntity()
