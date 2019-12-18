2f3b3fd4a3faa7d25e9128b4359e87fc9ea51e20
janfokkeurk <janfokkeurk@gmail.com>
2019-12-18 11:37:07 +0100

    merge

--- TypTop.GameEngine/Game.cs
+++ TypTop.GameEngine/Game.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Channels;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -13,15 +12,24 @@ namespace TypTop.GameEngine
 {
     public abstract class Game : IEnumerable<Entity>
     {
-        public readonly Random Rnd = new Random(DateTime.Now.Millisecond);
-        HashSet<Entity> _entities = new HashSet<Entity>();
-
-        public event TextCompositionEventHandler TextInput;
-
         public const double Width = 1920;
         public const double Height = 1080;
 
         private readonly HashSet<ITimed> _timedObjects = new HashSet<ITimed>();
+        public readonly Random Rnd = new Random(DateTime.Now.Millisecond);
+        private HashSet<Entity> _entities = new HashSet<Entity>();
+
+        public IEnumerator<Entity> GetEnumerator()
+        {
+            return _entities.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public event TextCompositionEventHandler TextInput;
 
         public ITimer AddTimer(Action callback, int interval)
         {
@@ -30,7 +38,8 @@ namespace TypTop.GameEngine
             return gameTimer;
         }
 
-        public void AddDelayedAction(Action callback, int millisecondsDelay, CancellationToken cancellationToken = default)
+        public void AddDelayedAction(Action callback, int millisecondsDelay,
+            CancellationToken cancellationToken = default)
         {
             _timedObjects.Add(new DelayedAction(callback, millisecondsDelay, cancellationToken));
         }
@@ -39,22 +48,17 @@ namespace TypTop.GameEngine
         {
             List<ITimed> removedTimers = null;
             foreach (ITimed timedObject in _timedObjects.ToList())
-            {
                 if (timedObject.IncrementTime(deltaTime))
                 {
-                    if(removedTimers == null)
+                    if (removedTimers == null)
                         removedTimers = new List<ITimed>();
 
                     removedTimers.Add(timedObject);
                 }
-            }
+
             if (removedTimers != null)
-            {
                 foreach (ITimed removedTimer in removedTimers)
-                {
                     _timedObjects.Remove(removedTimer);
-                }
-            }
         }
 
         public void AddEntity(Entity entity)
@@ -67,6 +71,7 @@ namespace TypTop.GameEngine
         {
             return _entities.Remove(entity);
         }
+
         public int RemoveEntity<TEntity>() where TEntity : Entity
         {
             return _entities.RemoveWhere(e => e is TEntity);
@@ -74,41 +79,21 @@ namespace TypTop.GameEngine
 
         public IEnumerable<Entity> GetEntitiesWithComponent<TComponent>() where TComponent : Component
         {
-            foreach (var entity in _entities)
-            {
+            foreach (Entity entity in _entities.ToList())
                 if (entity.HasComponent<TComponent>())
-                {
                     yield return entity;
-                }
-            }
         }
 
         public virtual void Update(float deltaTime)
         {
-            foreach (Entity entity in _entities.ToList())
-            {
-                entity.Update(deltaTime);
-            }
+            foreach (Entity entity in _entities.ToList()) entity.Update(deltaTime);
 
             RunTimedObjects(deltaTime);
         }
 
         public void Draw(DrawingContext drawingContext)
         {
-            foreach (Entity entity in _entities.ToList())
-            {
-                entity.Draw(drawingContext);
-            }
-        }
-
-        public IEnumerator<Entity> GetEnumerator()
-        {
-            return _entities.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
+            foreach (Entity entity in _entities.ToList()) entity.Draw(drawingContext);
         }
 
         public virtual void OnTextInput(TextCompositionEventArgs e)
@@ -119,9 +104,7 @@ namespace TypTop.GameEngine
         public void OnMouseDown(Point point)
         {
             foreach (Entity entity in GetEntitiesWithComponent<ClickComponent>())
-            {
                 entity.GetComponent<ClickComponent>().CaptureClick(point);
-            }
         }
     }
 }
